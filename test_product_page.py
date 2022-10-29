71f9b6efef3e34b33a4c1ecce66ffce5f6185f00
Olga Khvorostyna <olga.khvorostyna@gmail.com>
2022-10-29 13:31:42 +0300

    test_guest_can_add_product_to_basket parametrized

--- test_product_page.py
+++ test_product_page.py
@@ -1,10 +1,14 @@
 from .pages.base_page import BasePage
 from .pages.product_page import ProductPage
-from selenium.common.exceptions import NoAlertPresentException
+import pytest
 
-def test_add_to_cart_on_product_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
-    page = ProductPage(browser, link)
+@pytest.mark.parametrize('link', ["?promo=offer0", "?promo=offer1", "?promo=offer2",
+                                  "?promo=offer3", "?promo=offer4", "?promo=offer5",
+                                  "?promo=offer6", pytest.param("?promo=offer7", marks=pytest.mark.xfail),
+                                  "?promo=offer8", "?promo=offer9"])
+def test_guest_can_add_product_to_basket(browser, link):
+    product_link = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/{link}"
+    page = ProductPage(browser, product_link)
     page.open()
     page.add_to_cart()
     page.solve_quiz_and_get_code()
