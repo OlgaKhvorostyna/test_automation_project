2dbcad90dfb3e28febcc1127e8a6bef9e278b0ce
Olga Khvorostyna <olga.khvorostyna@gmail.com>
2022-10-31 14:29:54 +0200

    two tests connected with login were added

--- test_product_page.py
+++ test_product_page.py
@@ -1,9 +1,9 @@
 from .pages.base_page import BasePage
 from .pages.product_page import ProductPage
+from .pages.login_page import LoginPage
 from .pages.locators import ProductPageLocators
 import pytest
 
-"""
 @pytest.mark.parametrize('link', ["?promo=offer0", "?promo=offer1", "?promo=offer2",
                                   "?promo=offer3", "?promo=offer4", "?promo=offer5",
                                   "?promo=offer6", pytest.param("?promo=offer7", marks=pytest.mark.xfail),
@@ -15,7 +15,6 @@ def test_guest_can_add_product_to_basket(browser, link):
     page.add_to_cart()
     page.solve_quiz_and_get_code()
     page.should_be_product_in_cart()
-    """
 
 @pytest.mark.xfail
 def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
@@ -39,6 +38,19 @@ def test_message_disappeared_after_adding_product_to_basket(browser):
     page.add_to_cart()
     page.should_be_disappeared_the_success_message_after_adding_product_to_basket()
 
+def test_guest_should_see_login_link_on_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_be_login_link()
+
+def test_guest_can_go_to_login_page_from_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.go_to_login_page()
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_page()
 
 
 
